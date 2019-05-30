d0073953a80791ad8f2cb51e5ae750b15e922707
Unknown <thenjyon@googlemail.com>
2019-05-30 15:31:47 +0200

    Bester Generation & "Best path" gets highlightet

--- Assets/MachineLearning/Source/MachineLearning.cs
+++ Assets/MachineLearning/Source/MachineLearning.cs
@@ -2,40 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MachineLearning : MonoBehaviour
-{
+public class MachineLearning : MonoBehaviour {
     private Population population;
     public int populationCount = 1000;
     public Mesh mesh;
     public Material mat;
     public GameObject gO_goal;
+    public LineRenderer lr;
 
     Vector2 goal;
-    void Start()
-    {
+    void Start() {
+        lr = GetComponent<LineRenderer>();
         goal = new Vector2(gO_goal.transform.position.x, gO_goal.transform.position.y);
         population = new Population(populationCount, goal);
     }
 
-    void FixedUpdate()
-    {
-        if (population.AllDotsDead())
-        {
+    void FixedUpdate() {
+        if (population.AllDotsDead()) {
             population.CalculateFitness();
+            population.SetBestDot();
+            SetupLineRenderer();
             population.NaturalSelection();
             population.MutateDemBabies();
         }
-        else
-        {
+        else {
             population.Update();
         }
     }
 
-    void Update()
-    {
-        for(int i = 0; i < population.dots.Length; i++)
-        {
+    void Update() {
+        for (int i = 0; i < population.dots.Length; i++) {
             Graphics.DrawMesh(mesh, Matrix4x4.TRS(population.dots[i].pos, Quaternion.identity, Vector3.one * 2), mat, 0);
+        }
+    }
+    void SetupLineRenderer() {
+        lr.positionCount = population.dots[population.bestDot].pLastStepIndex;
+        int i = 0;
+        Vector2 pos = new Vector2(161, -170);
+        Vector2 vel = Vector2.zero;
+        while(i < lr.positionCount) {
+            vel += population.dots[population.bestDot].brain.directions[i];
+            vel = Vector2.ClampMagnitude(vel, 5);
+            pos += vel;
+            lr.SetPosition(i, pos);
+            ++i;
         }
     }
 }
--- Assets/MachineLearning/Source/Population.cs
+++ Assets/MachineLearning/Source/Population.cs
@@ -4,57 +4,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class Population
-{
+public class Population {
     public Dot[] dots;
 
     private float fitnessSum;
     private int gen = 1;
 
-    private int bestDot = 0;
+    public int bestDot = 0;
 
     private int minStep = 1000;
     Vector2 goal;
 
-    public Population(int size, Vector2 goal)
-    {
+    public Population(int size, Vector2 goal) {
         this.goal = goal;
         dots = new Dot[size];
-        for (int i = 0; i < size; i++)
-        {
+        for (int i = 0; i < size; i++) {
             dots[i] = new Dot(this.goal);
         }
     }
 
-    public void Update()
-    {
-        for (int i = 0; i < dots.Length; i++)
-        {
-            if (dots[i].brain.step > minStep)
-            {
+    public void Update() {
+        for (int i = 0; i < dots.Length; i++) {
+            if (dots[i].brain.step > minStep) {
+                dots[i].pLastStepIndex = dots[i].brain.step;
                 dots[i].dead = true;
             }
-            else
-            {
+            else {
                 dots[i].Update();
             }
         }
     }
 
-    public void CalculateFitness()
-    {
-        for (int i = 0; i < dots.Length; i++)
-        {
+    public void CalculateFitness() {
+        for (int i = 0; i < dots.Length; i++) {
             dots[i].CalculateFitness();
         }
     }
 
-    public bool AllDotsDead()
-    {
-        for (int i = 0; i < dots.Length; i++)
-        {
-            if (!dots[i].dead && !dots[i].reachedGoal)
-            {
+    public bool AllDotsDead() {
+        for (int i = 0; i < dots.Length; i++) {
+            if (!dots[i].dead && !dots[i].reachedGoal) {
                 return false;
             }
         }
@@ -62,17 +51,26 @@ public class Population
         return true;
     }
 
-    public void NaturalSelection()
-    {
+    public void NaturalSelection() {
         Dot[] newDots = new Dot[dots.Length];//next gen
-        SetBestDot();
         CalculateFitnessSum();
 
-        //the champion lives on 
-        newDots[0] = dots[bestDot].GimmeBaby();
-        newDots[0].isBest = true;
-        for (int i = 1; i < newDots.Length; i++)
-        {
+        int campoinKidz = 5;
+        if (dots[bestDot].reachedGoal && dots[bestDot].parentReachedGoal) campoinKidz = 20; 
+        else if (dots[bestDot].reachedGoal ) campoinKidz = 10; 
+        // Champion has more kids
+        for (int i = 0; i < campoinKidz; i++) {
+            if (i == 0) {
+                //the champion lives on 
+                newDots[0] = dots[bestDot].GimmeBaby();
+                newDots[0].isBest = true;
+            }
+            else {
+                newDots[i] = dots[bestDot].GimmeBaby();
+            }
+            if (dots[bestDot].reachedGoal) newDots[i].parentReachedGoal = true;
+        }
+        for (int i = campoinKidz; i < newDots.Length; i++) {
             //select parent based on fitness
             Dot parent = SelectParent(dots[bestDot].fitness);
 
@@ -86,25 +84,27 @@ public class Population
         gen++;
     }
 
-    public void CalculateFitnessSum()
-    {
+    public void CalculateFitnessSum() {
         fitnessSum = 0;
-        for (int i = 0; i < dots.Length; i++)
-        {
+        for (int i = 0; i < dots.Length; i++) {
             fitnessSum += dots[i].fitness;
         }
     }
 
-    Dot SelectParent(float bestfitness)
-    {
-        float rand = Random.Range(bestfitness/2, bestfitness);
-        Debug.Log("FitnessSum::" + fitnessSum + " Rand::" + rand);
+    Dot SelectParent(float bestfitness) {
+        float rand = Random.Range(bestfitness / 2, bestfitness);
+        if (dots[bestDot].reachedGoal) rand = Random.Range(bestfitness / 5, bestfitness);
+        //Debug.Log("BestFitness::" + bestfitness + " Rand::" + rand + " RandIndex::" + randIndex);
 
+        // Slows down the process A LOT ... maybe use List?
+        Array.Sort(dots, (a, b) => b.fitness.CompareTo(a.fitness));
 
-        for (int i = 0; i < dots.Length; i++)
-        {
-            if (dots[i].fitness > rand)
-            {
+        List<List<Vector2>> bestResult = new List<List<Vector2>>();
+        for (int i = 0; i < 100; i++) { bestResult.Add(new List<Vector2>(dots[i].brain.directions)); }
+
+        int randIndex = Random.Range(0, bestResult.Count);
+        for (int i = randIndex; i < bestResult.Count; i++) {
+            if (dots[i].fitness > rand) {
                 return dots[i];
             }
         }
@@ -114,23 +114,18 @@ public class Population
         return new Dot(this.goal);
     }
 
-    public void MutateDemBabies()
-    {
-        for (int i = 1; i < dots.Length; i++)
-        {
-            if(dots[i].parentReachedGoal) dots[i].brain.Mutate(0.01f);
+    public void MutateDemBabies() {
+        for (int i = 1; i < dots.Length; i++) {
+            if (dots[i].parentReachedGoal) dots[i].brain.Mutate(0.01f);
             else dots[i].brain.Mutate(0.1f);
         }
     }
 
-    public void SetBestDot()
-    {
+    public void SetBestDot() {
         float max = 0;
         int maxIndex = 0;
-        for (int i = 0; i < dots.Length; i++)
-        {
-            if (dots[i].fitness > max)
-            {
+        for (int i = 0; i < dots.Length; i++) {
+            if (dots[i].fitness > max) {
                 max = dots[i].fitness;
                 maxIndex = i;
             }
