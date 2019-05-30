d403f89fab80fd1e87451a140d14e9e3647ff6f0
Unknown <thenjyon@googlemail.com>
2019-05-30 16:19:05 +0200

    ADDED >> Singelton Class

---
+++ Assets/GloabalTools/Singelton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Singelton<T> : MonoBehaviour  where T : MonoBehaviour{
+    private static bool shuttingDown = false;
+    private static object lockObj = new object();
+    private static T instance;
+
+    public static T Instance {
+        get {
+            if (shuttingDown) {
+                Debug.LogError("[Singelton::Get] Instance '" + typeof(T) + "' destroyed!");
+                return null;
+            }
+            lock (lockObj) {
+                if (!instance) instance = (T)FindObjectOfType(typeof(T));
+                if (!instance) {
+                    var singletonObj = new GameObject();
+                    instance = singletonObj.AddComponent<T>();
+                    singletonObj.name = typeof(T).ToString() + " (Singelton)";
+                    DontDestroyOnLoad(singletonObj);
+                }
+            }
+            return instance;
+        }
+    }
+}
